fcf59d9ba6a62782a69e52f7339d51f0bdc558a5
eszterTari <tari.eszter.eng@gmail.com>
2018-11-06 13:29:17 +0100

    all tasks are implemented

--- src/main/java/com/codecool/Streams.java
+++ src/main/java/com/codecool/Streams.java
@@ -1,5 +1,8 @@
 package com.codecool;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.List;
@@ -13,22 +16,27 @@ public class Streams {
          */
 
         String[] a1 = {"one", "two", "three"};
-        Stream<String> s1 = /* ??? */;
+        Stream<String> s1 = Arrays.stream(a1);
+        s1.forEach(s-> System.out.println(s));
 
         /*
          * Create a stream containing the Strings "one" , "two" and "three"
          * without using an array
          */
 
-        Stream<String> s2 = /* ??? */;
+        Stream<String> s2 = Stream.of(a1);
+        s2.forEach(s-> System.out.println(s));
 
         /*
          * Create a stream using a stream builder.
          */
 
-        Stream.Builder<String> b1 = /* ??? */;
-        /* ??? */
-        Stream<String> s3 = /* ??? */;
+        Stream.Builder<String> b1 = Stream.builder();
+        b1.add("one");
+        b1.add("two");
+        b1.add("three");
+        Stream<String> s3 = b1.build();
+        s3.forEach(s-> System.out.println(s));
 
         /*
          * Collect one of the above defined streams into a list.
@@ -36,7 +44,10 @@ public class Streams {
          * HINT: the keyword here is "collect"
          */
 
-        List<String> l1 = /* ??? */;
+        Stream<String> sNew = Stream.of(a1);
+        List<String> l1 = sNew.collect(Collectors.toList());
+        l1.forEach(l-> System.out.println(l));
+
 
         /*
          * Streams can be infinite.  We obviously cannot create such a
@@ -54,13 +65,14 @@ public class Streams {
 
         Integer twoToTheZeroth = 1;
         UnaryOperator<Integer> doubler = (Integer x) -> 2 * x;
-        Stream<Integer> s4 = /* ??? */;
+        Stream<Integer> s4 = Stream.iterate(twoToTheZeroth, doubler).limit(20);
 
         /*
          * Create a stream containing the first ten elements of s4.
          */
 
-        Stream<Integer> s5 = s4. /* ??? */;
+        Stream<Integer> s5 = s4.limit(10);
+        s5.forEach(s-> System.out.println(s));
 
         /*
          * Create a stream containing the elements of the Fibonacci
@@ -70,6 +82,22 @@ public class Streams {
          */
 
         Supplier<Integer> fibSupp = new Fibonacci();
-        Stream<Integer> s6 = /* ??? */;
+        Stream<Integer> s6Temp = Stream.generate(fibSupp);
+        Stream<Integer> s6 = Stream.concat(Stream.of(0, 1), s6Temp.limit(20));
+        s6.forEach(System.out::println);
+    }
+
+    private static class Fibonacci implements Supplier<Integer> {
+        private Integer firstNumber = 0;
+        private Integer secondNumber = 1;
+
+        @Override
+        public Integer get() {
+            Integer nextNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+            return nextNumber;
+        }
+
     }
 }
